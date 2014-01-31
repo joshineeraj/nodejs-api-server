f5b870c029d341bf1423c00d663a5a5e4d7762ca
Neeraj Joshi <neeraj.joshi@3pillarglobal.com>
2014-01-31 13:12:31 +0530

    Adding files for server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    wine = require('./routes/wines');
+ 
+var app = express();
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+ 
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app['delete']('/wines/:id', wine.deleteWine);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
