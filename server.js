dada885f797bba7300a76e6bbd93fecc993fa74f
Neeraj Joshi <neeraj.joshi@3pillarglobal.com>
2014-02-14 18:42:45 +0545

    Adding fully qualified domain name resume.app for localhost:8000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.configure(function () {
 });
 
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Origin", "http://resume.app");
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
